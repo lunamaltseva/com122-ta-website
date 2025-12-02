7444e2c62d3d3ded9812bfd9c739fde196a98ac7
Luna Maltseva <md12366@auca.kg>
2025-12-02 09:24:44 +0600

    update schedule dec 2

--- navbar.js
+++ navbar.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const footer = document.getElementById("last-updated");
     if (footer) {
-        footer.textContent = "2025-11-10 15:08:43";
+        footer.textContent = "2025-12-02 09:24:28";
     }
 });
 
