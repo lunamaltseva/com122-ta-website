441ebc659e7cebc6e46516a80b1d735ec96c3e86
Luna <md12366@auca.kg>
2025-12-26 16:21:39 +0600

    Update website 26 dec

--- navbar.js
+++ navbar.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const footer = document.getElementById("last-updated");
     if (footer) {
-        footer.textContent = "2025-12-08 15:49:02";
+        footer.textContent = "2025-12-26 16:21:32";
     }
 });
 
